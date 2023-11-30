be7141db1e190fe440c6aabb2ca3c9d5b7cb63ab
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2023-11-30 19:49:26 +0530

    added diagnosis.py: implemented naivebayes

---
+++ diagnosis.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import sys
+import os.path
+
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
+TEST_SIZE = 0.4
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit('Usage:python diagnosis.py directory')
+
+    evidence, label = load_data(sys.argv[1])
+    X_train, x_test, Y_train, y_test = train_test_split(
+        evidence, label, test_size=TEST_SIZE)
+    
+    model = train_model(X_train, Y_train)
+    predictions = model.predict(x_test)
+
+    # print results
+    print(f'Correct: {(y_test == predictions).sum()}')
+    print(f'Incorrect: {(y_test != predictions).sum()}')
+
+
+def load_data(directory):
+    '''
+    load data from a dataset csv file into a list of symptoms and diseases.
+    return a tuple(evidence, labels)
+    '''
+    df = pd.read_csv(directory + os.path.sep + 'dataset.csv')
+
+    # clean the data: fix labels and remove null
+    cols = df.columns
+    data = df[cols].to_numpy().flatten()
+
+    s = pd.Series(data)
+    s = s.str.strip()
+    s = s.values.reshape(df.shape)
+
+    df = pd.DataFrame(s, columns=df.columns)
+
+    df = df.fillna(0)
+
+    # read Symptom-severity
+    df_s = pd.read_csv(directory + os.path.sep + 'Symptom-severity.csv')
+
+    # replace symptom name with severity
+    symptoms = df_s['Symptom']
+    for i in range(len(symptoms)):
+        weight = df_s['weight'][i]
+        df = df.replace(symptoms[i], weight)
+
+    # replace 0 for values not in Symptom-severity(unknown severity)
+    df = df.replace('dischromic _patches', 0)
+    df = df.replace('spotting_ urination', 0)
+    df = df.replace('foul_smell_of urine', 0)
+
+    evidence = df.iloc[:,1:].values
+    labels = df['Disease'].values
+
+    return (evidence, labels)
+
+def train_model(evidence, labels):
+    '''train model using naive bayes classifier'''
+    model = GaussianNB()
+    return model.fit(evidence, labels)
+
+if __name__ == '__main__':
+    main()

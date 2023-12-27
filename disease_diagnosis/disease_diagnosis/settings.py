b4e284d855ad3118a15ed8251d6a749faf4ee993
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2023-12-27 21:22:13 +0530

    added users app

--- disease_diagnosis/disease_diagnosis/settings.py
+++ disease_diagnosis/disease_diagnosis/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'diagnosis_system.apps.DiagnosisSystemConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
